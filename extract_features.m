b5877551c2771d553028e71dbf9d8cbcc22da871
David Valley <31631465+drv34@users.noreply.github.com>
2018-04-24 00:18:05 -0400

    bandpower data

--- extract_features.m
+++ extract_features.m
@@ -1,13 +1,15 @@
 function f = extract_features(data, num_columns)
     s = size(data);
+    beta = [12, 30];
     num_channels = s(2);
-    num_features = 2;
+    num_features = 1;
     features = zeros(1, num_columns);
     
     for channel = 1:num_channels
         feature_index = (channel-1)*num_features + 1;
-        features(feature_index) = line_length(data, channel);
-        features(feature_index + 1) = var(data(:,channel));
+        %features(feature_index) = line_length(data, channel);
+        %features(feature_index) = var(data(:,channel));
+        features(feature_index) = bandpower(data(:,channel), s(1), beta);
     end
     
     f = features;
--- load_file.m
+++ load_file.m
@@ -1,4 +1,4 @@
-num_features = 2;
+num_features = 1;
 
 % patient 1
 num_columns = 96 * num_features + 1;
@@ -16,19 +16,17 @@ for i = 219:818
     patient_1(i, :) = extract_features(patient_data, num_columns);
     patient_1(i,num_columns) = 0;
 end
-
-% patient 1 test
-num_columns = num_columns - 1;
-patient_1_test = zeros(931, num_columns);
-for i = 1:931
-    filename = sprintf ('../data/patient_1/test/patient_1_test_%d.mat', i);
-    patient_data = importdata(filename);
-    patient_1_test(i, :) = extract_features(patient_data, num_columns);
-end
-
 save('features/patient_1.mat', 'patient_1');
-save('features/patient_1_test.mat', 'patient_1_test');
 
+% % patient 1 test
+% num_columns = num_columns - 1;
+% patient_1_test = zeros(931, num_columns);
+% for i = 1:931
+%     filename = sprintf ('../data/patient_1/test/patient_1_test_%d.mat', i);
+%     patient_data = importdata(filename);
+%     patient_1_test(i, :) = extract_features(patient_data, num_columns);
+% end
+% save('features/patient_1_test.mat', 'patient_1_test');
 
 % patient 2
 num_columns = 56 * num_features + 1;
@@ -49,15 +47,15 @@ end
 save('features/patient_2.mat', 'patient_2');
 
 
-% patient 2 test
-num_columns = num_columns - 1;
-patient_2_test = zeros(1242, num_columns);
-for i = 1:1242
-    filename = sprintf ('../data/patient_2/test/patient_2_test_%d.mat', i);
-    patient_data = importdata(filename);
-    patient_2_test(i, :) = extract_features(patient_data, num_columns);
-end
-save('features/patient_2_test.mat', 'patient_2_test');
+% % patient 2 test
+% num_columns = num_columns - 1;
+% patient_2_test = zeros(1242, num_columns);
+% for i = 1:1242
+%     filename = sprintf ('../data/patient_2/test/patient_2_test_%d.mat', i);
+%     patient_data = importdata(filename);
+%     patient_2_test(i, :) = extract_features(patient_data, num_columns);
+% end
+% save('features/patient_2_test.mat', 'patient_2_test');
 
 
 % patient 3
@@ -78,15 +76,15 @@ for i = 297:1196
 end
 save('features/patient_3.mat', 'patient_3');
 
-% patient 3 test
-num_columns = num_columns - 1;
-patient_3_test = zeros(2400, num_columns);
-for i = 1:2400
-    filename = sprintf ('../data/patient_3/test/patient_3_test_%d.mat', i);
-    patient_data = importdata(filename);
-    patient_3_test(i, :) = extract_features(patient_data, num_columns);
-end
-save('features/patient_3_test.mat', 'patient_3_test');
+% % patient 3 test
+% num_columns = num_columns - 1;
+% patient_3_test = zeros(2400, num_columns);
+% for i = 1:2400
+%     filename = sprintf ('../data/patient_3/test/patient_3_test_%d.mat', i);
+%     patient_data = importdata(filename);
+%     patient_3_test(i, :) = extract_features(patient_data, num_columns);
+% end
+% save('features/patient_3_test.mat', 'patient_3_test');
 
 
 % patient 4
@@ -107,15 +105,15 @@ for i = 425:1624
 end
 save('features/patient_4.mat', 'patient_4');
 
-% patient 4 test
-num_columns = num_columns - 1;
-patient_4_test = zeros(2184, num_columns);
-for i = 1:2184
-    filename = sprintf ('../data/patient_4/test/patient_4_test_%d.mat', i);
-    patient_data = importdata(filename);
-    patient_4_test(i, :) = extract_features(patient_data, num_columns);
-end
-save('features/patient_4_test.mat', 'patient_4_test');
+% % patient 4 test
+% num_columns = num_columns - 1;
+% patient_4_test = zeros(2184, num_columns);
+% for i = 1:2184
+%     filename = sprintf ('../data/patient_4/test/patient_4_test_%d.mat', i);
+%     patient_data = importdata(filename);
+%     patient_4_test(i, :) = extract_features(patient_data, num_columns);
+% end
+% save('features/patient_4_test.mat', 'patient_4_test');
 
 
 % patient 5
@@ -136,15 +134,15 @@ for i = 181:1980
 end
 save('features/patient_5.mat', 'patient_5');
 
-% patient 5 test
-num_columns = num_columns - 1;
-patient_5_test = zeros(3057, num_columns);
-for i = 1:3057
-    filename = sprintf ('../data/patient_5/test/patient_5_test_%d.mat', i);
-    patient_data = importdata(filename);
-    patient_5_test(i, :) = extract_features(patient_data, num_columns);
-end
-save('features/patient_5_test.mat', 'patient_5_test');
+% % patient 5 test
+% num_columns = num_columns - 1;
+% patient_5_test = zeros(3057, num_columns);
+% for i = 1:3057
+%     filename = sprintf ('../data/patient_5/test/patient_5_test_%d.mat', i);
+%     patient_data = importdata(filename);
+%     patient_5_test(i, :) = extract_features(patient_data, num_columns);
+% end
+% save('features/patient_5_test.mat', 'patient_5_test');
 
 
 % patient 6
@@ -165,15 +163,15 @@ for i = 314:2413
 end
 save('features/patient_6.mat', 'patient_6');
 
-% patient 6 test
-num_columns = num_columns - 1;
-patient_6_test = zeros(4116, num_columns);
-for i = 1:4116
-    filename = sprintf ('../data/patient_6/test/patient_6_test_%d.mat', i);
-    patient_data = importdata(filename);
-    patient_6_test(i, :) = extract_features(patient_data, num_columns);
-end
-save('features/patient_6_test.mat', 'patient_6_test');
+% % patient 6 test
+% num_columns = num_columns - 1;
+% patient_6_test = zeros(4116, num_columns);
+% for i = 1:4116
+%     filename = sprintf ('../data/patient_6/test/patient_6_test_%d.mat', i);
+%     patient_data = importdata(filename);
+%     patient_6_test(i, :) = extract_features(patient_data, num_columns);
+% end
+% save('features/patient_6_test.mat', 'patient_6_test');
 
 
 % patient 7
@@ -194,12 +192,12 @@ for i = 308:2707
 end
 save('features/patient_7.mat', 'patient_7');
 
-% patient 7 test
-num_columns = num_columns - 1;
-patient_7_test = zeros(4523, num_columns);
-for i = 1:4523
-    filename = sprintf ('../data/patient_7/test/patient_7_test_%d.mat', i);
-    patient_data = importdata(filename);
-    patient_7_test(i, :) = extract_features(patient_data, num_columns);
-end
-save('features/patient_7_test.mat', 'patient_7_test');
+% % patient 7 test
+% num_columns = num_columns - 1;
+% patient_7_test = zeros(4523, num_columns);
+% for i = 1:4523
+%     filename = sprintf ('../data/patient_7/test/patient_7_test_%d.mat', i);
+%     patient_data = importdata(filename);
+%     patient_7_test(i, :) = extract_features(patient_data, num_columns);
+% end
+% save('features/patient_7_test.mat', 'patient_7_test');
