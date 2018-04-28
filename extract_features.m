0eada8f7d8d9d9568639c742045ff8aeff7c487c
David Valley <31631465+drv34@users.noreply.github.com>
2018-04-28 18:02:25 -0400

    updated many files

--- extract_features.m
+++ extract_features.m
@@ -4,14 +4,14 @@ function f = extract_features(data, num_columns)
     num_channels = s(2);
     num_features = 1;
     features = zeros(1, num_columns);
-    
+
     for channel = 1:num_channels
         feature_index = (channel-1)*num_features + 1;
-        %features(feature_index) = line_length(data, channel);
+        features(feature_index) = line_length(data, channel);
         %features(feature_index) = var(data(:,channel));
-        features(feature_index) = bandpower(data(:,channel), s(1), beta);
+        %features(feature_index) = bandpower(data(:,channel), s(1), beta);
     end
-    
+
     f = features;
 
 end
